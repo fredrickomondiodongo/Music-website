f10726ec264da224a6bcf65e4eaa2162df9f5d78
Fredy <142295949+fredrickomondiodongo@users.noreply.github.com>
2023-08-31 14:07:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ var myVideo = document.getElementById("myVideo");
 
 function stopVideo(){
     videoPlayer.style.display = "none";
-    
+     myVideo.muted = true;
 }
 function playVideo(file){
     myVideo.src = file;
     videoPlayer.style.display = "block";
+    myVideo.muted = false;
 }
